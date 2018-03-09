9355acdcfa2b2a83a3262267b1a32686a59620af
Henry Baxter <henry.baxter@gmail.com>
2018-03-08 16:30:37 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scanningegs',
-      version='0.0.3',
+      version='0.0.4',
       description='Generate EGSnrc files for a scanning target',
       url='http://github.com/henrybaxter/scanningegs',
       author='Henry Baxter',
